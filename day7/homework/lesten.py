4eb3784bd47030b12bdaaf80591c078083d2cf5f
davit <jiquridavit2@gmail.com>
2024-03-02 23:56:48 +0400

    commit

---
+++ day7/homework/lesten.py
@@ -0,0 +1,14 @@
+gio = 12
+nika = 24
+dato = 11
+toko = 10
+mate = 13
+
+print(gio * nika / dato + toko + mate)
+
+
+
+
+
+
+
---
+++ day8/homework/lesten.py
@@ -0,0 +1,3 @@
+dato  = int(input("enter your age"))
+gio = int(input("enter your age"))
+print(dato + gio)
