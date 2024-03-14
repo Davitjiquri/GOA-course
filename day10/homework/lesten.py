2e598e82f043dd1867757f2467540df7c7a480c0
davit <jiquridavit2@gmail.com>
2024-03-14 23:30:42 +0400

    commit

---
+++ day10/homework/lesten.py
@@ -0,0 +1,42 @@
+num_1 = 49
+num_2 = 37
+
+print(num_1 >= num_2)
+
+print(num_1 <= num_2)
+
+print(num_1 != num_2)
+
+print(num_1 == num_2)
+
+
+num_1 = 324
+num_2 = 123
+
+print(num_1 >= num_2)
+
+print(num_1 <= num_2)
+
+print(num_1 != num_2)
+
+print(num_1 == num_2)
+
+
+num_1 = 20
+num_2 = 193
+
+print(num_1 >= num_2)
+
+print(num_1 <= num_2)
+
+print(num_1 != num_2)
+
+print(num_1 == num_2)
+
+
+
+
+for i in range(0, 30):
+ print ("goa is the best")
+
+ for i in range(2, 50, 4):
